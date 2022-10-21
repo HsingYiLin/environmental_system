ec717629866331fd9fb18fdb1f169c02e622c7b4
hsingyi <zzzxxcvbn87@gmail.com>
2022-10-21 17:59:47 +0800

    modified crud

--- add_del_employee.js
+++ add_del_employee.js
@@ -30,124 +30,108 @@ var add_del_init = function () {
     stateInfo = document.getElementById("stateInfo");
     ad_tbody = document.getElementById("ad_tbody");
     director = document.getElementById("director");
-    loadTable();
+    actionDB("init");
     ad_add_del.setAttribute("selected", true);
     ad_chgpage.addEventListener("change", ad_changePage, false);
-    ad_confirm.addEventListener("click", addReqVal);
-    // up_confirm.addEventListener("click", updateReqVal);
-    // sel_confirm.addEventListener("click", selelectReqVal);
+    ad_confirm.addEventListener("click", function(){actionDB("add");});
+    sel_confirm.addEventListener("click", function(){actionDB("select");});
+    up_confirm.addEventListener("click", function(){actionDB("update")});
 
 }
 
-var loadTable = function (){
-    toSend = {pload: "init"};
-    jsonString = JSON.stringify(toSend);
-    // console.log("dynamicTable",jsonString);
-    // const xmlhttp =new XMLHttpRequest();
-    xmlhttp.open("POST" ,em_url);
+var actionDB = function(params) {
+    switch(params){
+
+        case "init":
+            toSend = {pload: "init"};
+            httpReqFun(toSend);
+            break;
+
+        case "add":
+            var state = ad_status.checked?"在職":"離職";
+            if(ad_startDate.value != "" && ad_name.value != ""){
+                toSend ={
+                    pload: "add",
+                    startDate: ad_startDate.value,
+                    name: ad_name.value,
+                    title: ad_title.value,
+                    state: state
+                };   
+                httpReqFun(toSend);
+            }else{
+                stateInfo.innerText = "到職日或名字不得為空";
+            }
+            break;
+
+        case "select":
+            if(ad_name.value != ""){
+                toSend ={
+                    pload: "select",
+                    name: ad_name.value
+                };
+                httpReqFun(toSend);
+            }else{
+                stateInfo.innerText = "名字不得為空";
+            }
+            break;
+
+        case "update":
+            var state = ad_status.checked?"在職":"離職";
+            if(ad_startDate.value != "" && ad_name.value != ""){
+                toSend ={
+                    pload: "update",
+                    startDate: ad_startDate.value,
+                    name: ad_name.value,
+                    title: ad_title.value,
+                    state: state
+                };   
+                httpReqFun(toSend);
+            }else{
+                stateInfo.innerText = "到職日或名字不得為空";
+            }
+            break;
+
+    }
+}
+
+var httpReqFun = function (param){
+    // console.log("1",param);
+    var arr_data;
+    jsonString = JSON.stringify(param);
+    xmlhttp.open("POST",em_url);
     xmlhttp.setRequestHeader("Content-Type","application/json");
-    xmlhttp.onreadystatechange = () =>{
+    xmlhttp.onreadystatechange = () => {
         if(xmlhttp.readyState === 4 && xmlhttp.status == 200){
-            data = xmlhttp.responseText;
-            loadFinish(data);
+            arr_data = JSON.parse(xmlhttp.responseText);
+            // console.log(arr_data);
+            setTimeout(function(){
+                stateInfo.innerText = "";
+            },3000);
+        
+            if(arr_data["status"] == "add success" || arr_data["status"] == "update success"){
+                actionDB("init");
+                clearInput();
+        
+            }else if(arr_data["status"] == "success!" || arr_data["status"] == "no data" ||arr_data["status"] == "select success" || arr_data["status"] == "duplicate" || arr_data["status"] == "update fail" ){
+                stateInfo.innerText = arr_data["status"];
+                parseAllData(arr_data);
+            }        
         }
     }
     xmlhttp.send(jsonString);
 }
 
-var addReqVal = function (){ 
-    var state = ad_status.checked?"在職":"離職";
-    var data = "";
-
-    if(ad_startDate.value != "" && ad_name.value != ""){
-        toSend ={
-            pload: "add",
-            startDate: ad_startDate.value,
-            name: ad_name.value,
-            title: ad_title.value,
-            state: state
-        };
-        
-        jsonString = JSON.stringify(toSend);
-        console.log(jsonString);
-        // xmlhttp = new XMLHttpRequest();//試試看遍全域
-        xmlhttp.open("POST",em_url);
-        xmlhttp.setRequestHeader("Content-Type","application/json");
-        xmlhttp.onreadystatechange = () => {
-            if(xmlhttp.readyState === 4 && xmlhttp.status == 200){
-                data = xmlhttp.responseText;
-                loadFinish(data);
-            }
-        }
-        xmlhttp.send(jsonString);
-    }else{
-        stateInfo.innerText = "到職日或名字不得為空";
-    }    
-}
-
-// var updateReqVal = function (e){
-//     toSend = {
-//         pload: "update",
-//         startDate: ad_startDate.value,
-//         name: ad_name.value,
-//         title: ad_title.value,
-//         state: state
-//     };
-//     // console.log(toSend);
-
-// var selelectReqVal = function (){
-//     if(ad_name.value != ""){
-//         toSend ={
-//             pload: "select",
-//             name: ad_name.value,
-//         };
-        
-//         jsonString = JSON.stringify(toSend);
-//         console.log(jsonString);
-//         xmlhttp = new XMLHttpRequest();//試試看遍全域
-//         xmlhttp.open("POST",em_url);
-//         xmlhttp.setRequestHeader("Content-Type","application/json");
-//         xmlhttp.onreadystatechange = () => {
-//             if(xmlhttp.readyState === 4 && xmlhttp.status == 200){
-//                 data = xmlhttp.responseText;
-//                 loadFinish(data);
-//             }
-//         }
-//         xmlhttp.send(jsonString);
-//     }else{
-//         stateInfo.innerText = "到職日或名字不得為空";
-//     }    
-// }
-
-var loadFinish = function (json){
-    var arr_data = JSON.parse(json);
-    console.log("loadFinish",arr_data);
-
-    setTimeout(function(){
-        stateInfo.innerText = "";
-    },3000);
-
-    if(arr_data["status"] == "add success"){
-        // parseTable(toSend);
-        loadTable();
-        clearInput();
-    }else if(arr_data["status"] == "success!"){
-        stateInfo.innerText = arr_data["status"];
-        parseAllData(arr_data);
-    }else if(arr_data["status"] == "duplicate"){
-        stateInfo.innerText = arr_data["status"];
-    }
-}
-
 var parseAllData = function (initData){
     ad_tableHTML = "";
     ad_tbody.innerHTML = "<tr class=first_tr><td>到職日</td><td>員工</td><td>職稱</td><td>狀態</td></tr>";
-    var data_size = Object.keys(initData["name"]).length;
-    for(var j = 1; j <= data_size; j++){
-        ad_tableHTML += "<tr class = datatr><td>"+initData.startdate[j]+"</td><td>"+initData.name[j]+"</td><td>"+initData.title[j]+"</td><td>"+initData.state[j]+"</td></tr>";
+    if(initData["state"] != "update fail" && initData["status"] != "no data" ){
+        var data_size = Object.keys(initData["name"]).length;
+        for(var j = 1; j <= data_size; j++){
+            ad_tableHTML += "<tr class = datatr><td>"+initData.startdate[j]+"</td><td>"+initData.name[j]+"</td><td>"+initData.title[j]+"</td><td>"+initData.state[j]+"</td></tr>";
+        }
+        ad_tbody.innerHTML += ad_tableHTML;
+        getAllElement();
     }
-    ad_tbody.innerHTML += ad_tableHTML;
-    getAllElement();
 }
 
 var ad_changePage = function (e) {
@@ -172,11 +156,3 @@ var getAllElement = function (){
         })
     }
 }
-
-// var parseTable = function (data){
-//     clearInput();
-//     ad_tableHTML = "";
-//     // console.log("parseTable",data);
-//     ad_tableHTML += "<tr ><td>"+data.startDate+"</td><td>"+data.name+"</td><td>"+data.title+"</td><td>"+data.state+"</td></tr>"
-//     ad_tbody.innerHTML += ad_tableHTML;
-// }
--- employee.php
+++ employee.php
@@ -2,12 +2,12 @@
 	header("Content-Type: application/json");
 	header("Access-Control-Allow-Origin: *");
 	header('Access-Control-Allow-Headers:x-requested-with,content-type'); 
-
 	$host = 'localhost';
 	$dbuser ='root';
 	$dbpassword = 'a12345678';
 	$dbname = 'mydb';
 	$mydb_link = mysqli_connect($host,$dbuser,$dbpassword,$dbname);
+
 	if($mydb_link->connect_error){
 		die('Connection failed: '.$mydb_link->connect_error);
 	}
@@ -15,30 +15,8 @@
 	$requestPayload = file_get_contents("php://input");
 	$object = json_decode($requestPayload, true);
   	$arr_res =Array();
-
-	if($object["pload"] == "add"){
-		$startDate = $object["startDate"];
-    	$name = $object["name"];
-    	$title = $object["title"];
-   	 	$state = $object["state"];
-
-		$sql_sel = "SELECT `name` FROM employee WHERE `name` =" ."'$name'";
-		$result = mysqli_query($mydb_link, $sql_sel);
-		if (mysqli_num_rows($result) > 0) {
-			$arr_res["status"] = "duplicate";
-			echo json_encode($arr_res);
-			die();
-		} 
-		
-		$sql_add = "INSERT INTO employee (`name`, `title`, `state`, `startDate`) VALUES (" ."'$name'". "," ."'$title'". "," ."'$state'". "," ."'$startDate'". ")";
-		if($mydb_link->query($sql_add) === TRUE){
-			$arr_res["status"] = "add success";
-			echo json_encode($arr_res);
-		} else {
-  			echo "Error: " . $sql_add . "<br>" . $mydb_link->error;
-		}
-
-	}else if($object["pload"] == "init"){
+	
+	if($object["pload"] == "init"){
 		$sql_init = "SELECT * FROM employee";
 		$result = mysqli_query($mydb_link, $sql_init);
 		$i=1;
@@ -56,5 +34,63 @@
 			$arr_res["status"] = "no data";
 			echo json_encode($arr_res);
 		}
+
+	}else if($object["pload"] == "add"){
+		$startDate = $object["startDate"];
+    	$name = $object["name"];
+    	$title = $object["title"];
+   	 	$state = $object["state"];
+		$sql_sel = "SELECT `name` FROM employee WHERE `name` =" ."'$name'";
+		$result = mysqli_query($mydb_link, $sql_sel);
+		if (mysqli_num_rows($result) > 0) {
+			$arr_res["status"] = "duplicate";
+			echo json_encode($arr_res);
+			die();
+		} 
+
+		$sql_add = "INSERT INTO employee (`name`, `title`, `state`, `startDate`) VALUES (" ."'$name'". "," ."'$title'". "," ."'$state'". "," ."'$startDate'". ")";
+		if($mydb_link->query($sql_add) === TRUE){
+			$arr_res["status"] = "add success";
+			echo json_encode($arr_res);
+		} else {
+			$arr_res["status"] = "add fail";
+			echo json_encode($arr_res);
+  			// echo "Error: " . $sql_add . "<br>" . $mydb_link->error;
+		}
+
+	}else if($object["pload"] == "select"){
+		$name = $object["name"];
+		$sql_sel2 = "SELECT * FROM employee WHERE `name` =" ."'$name'";
+		$result2 = mysqli_query($mydb_link, $sql_sel2);
+		$i=1;
+		if ($result2->num_rows > 0) {
+			while($row=mysqli_fetch_assoc($result2)){
+				$arr_res["name"][$i]=$row['name'];
+				$arr_res["title"][$i]=$row['title'];
+				$arr_res["state"][$i]=$row['state'];
+				$arr_res["startdate"][$i]=$row['startdate'];	
+				$i++;		
+			}
+			$arr_res["status"] = "select success";
+			echo json_encode($arr_res);
+		}else{
+			$arr_res["status"] = "no data";
+			echo json_encode($arr_res);
+		}
+	}else if($object["pload"] == "update"){
+		$startDate = $object["startDate"];
+    	$name = $object["name"];
+    	$title = $object["title"];
+   	 	$state = $object["state"];
+		$sql_up = "UPDATE employee SET `title` =" ."'$title'"." ,`state` =" ."'$state'" . " ,`startDate` =" . "'$startDate'" . "WHERE `name` =" ."'$name'";
+
+		if(mysqli_query($mydb_link, $sql_up) && $mydb_link->affected_rows > 0){
+			$arr_res["status"] = "update success";
+			// $arr_res["howMany"] = $mydb_link->affected_rows;
+		}else{
+			$arr_res["status"] = "update fail";
+			$arr_res["error"] =mysqli_error($mydb_link);
+		}
+		echo json_encode($arr_res);
 	}
 ?>
