36abf50b560872e33c3c5d77832169ea43257751
hsingyi <zzzxxcvbn87@gmail.com>
2022-11-24 17:54:28 +0800

    modified bug

--- test.js
+++
@@ -1,8 +0,0 @@
-
-// var test_init = function(){
-//     console.log("init");
-//     var set = info_tw("AAA")
-//     var aaa = document.getElementById("aaa");
-//     aaa.innerText = set;
-//     console.log(set);
-// }
